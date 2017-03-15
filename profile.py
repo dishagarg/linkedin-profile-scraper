85210da7c624627a4b36bf2cf717a3492d6f0709
Disha Garg <garg.disha8@gmail.com>
2017-03-15 04:30:58 -0700

    Updating files

--- profile.py
+++ profile.py
@@ -9,20 +9,24 @@ import json
 import getopt
 from LinkedinController import LinkedinController
 
+
 def main(params):
-	config_file = None
-	url = None
-	opts, args = getopt.getopt(params, "u:c:")
-	if opts:
-		for o, a in opts:
-			if o == "-c":
-				config_file = a
-			elif o == "-u":
-				url = a
-	linkedinTool = LinkedinController(config=config_file, debug=True)
-	profile = linkedinTool.extractProfile(url)
-	print json.dumps(profile)
+    config_file = None
+    url = None
+    opts, args = getopt.getopt(params, "u:c:")
+    if opts:
+        for o, a in opts:
+            if o == "-c":
+                config_file = a
+            elif o == "-u":
+                url = a
+    linkedin_tool = LinkedinController(config=config_file, debug=True)
+    profile = linkedin_tool.extractProfile(url)
+    with open("workfile.json", 'w') as f:
+        f.write(json.dumps(profile, sort_keys=True, indent=4, separators=(',', ': ')))
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
+    argv = sys.argv[1:]   # change to be done is here and in linkedin-config.
+    # type(argv) is a list
+    # print argv ['-u', 'http://'] or ['-u', 'http://', '-c', 'linkedin-config']
     main(argv)
